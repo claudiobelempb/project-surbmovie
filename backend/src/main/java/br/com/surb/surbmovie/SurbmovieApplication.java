6087a581dc5990941d98e0e887dfceb188dd1b38
claudiobelempb <claudio.c.lima@hotmail.com>
2023-09-14 09:55:21 -0300

    initial backend

---
+++ backend/src/main/java/br/com/surb/surbmovie/SurbmovieApplication.java
@@ -0,0 +1,13 @@
+package br.com.surb.surbmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurbmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurbmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/surb/surbmovie/SurbmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.surb.surbmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurbmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
