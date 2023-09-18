40f6f67f48b9b91717aa7807ccf67af18ec609c6
claudiobelempb <claudio.c.lima@hotmail.com>
2023-09-18 09:22:34 -0300

    fontend v1

--- frontend/src/components/AppFormCard/index.tsx
+++ frontend/src/components/AppFormCard/index.tsx
@@ -1,8 +1,9 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import axios, { AxiosRequestConfig } from 'axios';
+import { FormEvent, useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { BASE_URL } from '../../utils/requests';
 import { Movie } from '../../utils/types/Movie';
+import { validateEmail } from '../../utils/validate';
 import AppHeader from '../AppHeader';
 import {
   BoxButton,
@@ -23,12 +24,40 @@ type PropsMovie = {
 const AppFormCard: React.FC<PropsMovie> = ({ movieId }) => {
   const [movie, setMovie] = useState<Movie>();
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get(`${BASE_URL}/movies/${movieId}`).then(response => {
       const data = response.data as Movie;
       setMovie(data);
     });
   }, [movieId]);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const email = (event.target as any).email.value;
+    const score = (event.target as any).score.value;
+
+    if (!validateEmail(email)) {
+      return;
+    }
+
+    const config: AxiosRequestConfig = {
+      baseURL: BASE_URL,
+      method: 'PUT',
+      url: '/reviews',
+      data: {
+        email,
+        score,
+        movieId
+      }
+    };
+
+    axios(config).then(response => {
+      console.log(response.data);
+      navigate('/');
+    });
+  };
 
   return (
     <>
@@ -44,7 +73,7 @@ const AppFormCard: React.FC<PropsMovie> = ({ movieId }) => {
             <h2>{movie?.title}</h2>
             <p>Avaliação: {movie?.score}</p>
             <BoxForm>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <BoxInput>
                   <input
                     placeholder='Informe seu E-mail'
@@ -55,8 +84,8 @@ const AppFormCard: React.FC<PropsMovie> = ({ movieId }) => {
                   <input
                     placeholder='Informe sua avaliação'
                     type='number'
-                    name='avaliacao'
-                    id='avaliacao'
+                    name='score'
+                    id='score'
                     min='1'
                     max='5'
                   />
